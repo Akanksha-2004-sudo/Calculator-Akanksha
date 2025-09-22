ab0263bc1ff35e0d1f8f9a5c751ce1458ceef57b
PraveenD20 <dharavathpraveenjadav@gmail.com>
2025-09-22 16:52:01 +0530

    This is Multiplucation

---
+++ src/main/java/com/alpha/Calculator/Multiplication_Praveen.java
@@ -0,0 +1,18 @@
+package com.alpha.Calculator;
+import java.util.Scanner;
+
+
+public class Multiplication_Praveen {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a Number");
+		int n1=sc.nextInt();
+		System.out.println("Enter a Number");
+		int n2=sc.nextInt();
+		System.out.println("The Product of the given numbers id :"+" " +n1*n2);
+		
+	}
+
+     
+
+}
